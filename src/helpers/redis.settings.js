19af0133c76b65e3b6e70cd2d10e454b56cd05d4
TheRealJ-OnCode <therealjonweb@gmail.com>
2025-05-10 21:32:37 +0400

    redis config changed

--- src/helpers/redis.settings.js
+++ src/helpers/redis.settings.js
@@ -1,5 +1,12 @@
 const redis = require("redis");
-const redisClient = redis.createClient({});
-redisClient.connect();
-redisClient.on('error',err=>console.log('Redis Client error: ',err));
-module.exports  = redisClient
+require("dotenv").config();
+const redis_pass = process.env.REDIS_PASSWORD
+const client = redis.createClient({
+    url: `rediss://default:${redis_pass}@loyal-lark-15815.upstash.io:6379`
+});
+client.on("error", function (err) {
+    throw err;
+});
+ client.connect()
+// await client.set('foo', 'bar');
+module.exports = client
